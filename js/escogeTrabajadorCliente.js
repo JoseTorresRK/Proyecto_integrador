bd837cdbdd4bb884fa19894b18ecea8643ce804d
Osmar Ramos <axl090696@gmail.com>
2021-08-25 22:11:06 -0500

    25/08/21 FIX | Sangría Código
    Sagría y acomodo del Código

--- js/escogeTrabajadorCliente.js
+++ js/escogeTrabajadorCliente.js
@@ -1,23 +1,21 @@
-
-    const perfil=document.querySelectorAll("#navbarNav  .dropdown-item ");
-    const mylocal=window.localStorage;
-    const paginaActual=document.getElementsByTagName("title")[0].childNodes[0];
-    console.log(perfil);
-    console.log(paginaActual)
-    perfil[0].onclick=function(){
-        mylocal.setItem("Bandera",[true]);
-        definirPagina(paginaActual.baseURI)
-    }   
-    perfil[1].onclick=function(){
-        mylocal.setItem("Bandera",[false]);
-        definirPagina(paginaActual.baseURI)
-    }
-function definirPagina(paginaActual){
-    console.log(typeof(paginaActual));
-    if(paginaActual.includes("index.html")){
-        window.location="./html/formulario.html";
-    }
-    else{
-        window.location="./formulario.html";
-    }
+const perfil = document.querySelectorAll("#navbarNav  .dropdown-item ");
+const mylocal = window.localStorage;
+const paginaActual = document.getElementsByTagName("title")[0].childNodes[0];
+console.log(perfil);
+console.log(paginaActual);
+perfil[0].onclick = function () {
+  mylocal.setItem("Bandera", [true]);
+  definirPagina(paginaActual.baseURI);
+};
+perfil[1].onclick = function () {
+  mylocal.setItem("Bandera", [false]);
+  definirPagina(paginaActual.baseURI);
+};
+function definirPagina(paginaActual) {
+  console.log(typeof paginaActual);
+  if (paginaActual.includes("index.html")) {
+    window.location = "./html/formulario.html";
+  } else {
+    window.location = "./formulario.html";
+  }
 }
