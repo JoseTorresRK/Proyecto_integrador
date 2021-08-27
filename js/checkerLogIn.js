a64c769d5fd00c6753fdd88960e7f8ea12092103
Oscar Mtz de la O <oscarmtz.delao@gmail.com>
2021-08-27 01:25:40 -0500

    feat && refactor
    Ya se puede tenr otra navbar dependiendo del inicio de sesion, solo se cmabio en index.html y registro.html la navbar, no se acomoda hacia la derecha adecuadamente

---
+++ js/checkerLogIn.js
@@ -0,0 +1,49 @@
+/**
+ * @function cambia el estado de la barra de navegacion dependiendo si existe un boleano en window.localStorage.loggedIn
+ */
+function checkerLogIn(){
+    if(window.localStorage.loggedIn === "true"){
+        mostrarNavBar(false,"loggedIn");
+    }else if(window.localStorage.loggedIn === "false"){
+        mostrarNavBar(true,"loggedOut");
+    }
+}
+
+
+/**
+ * 
+ * @param {boolean} booleano Es el valor contrario del que se tiene en window.localStorage.loggedIn
+ * @param {*} typeOfLog Será la clase que si se muestre dependeindo si se inicio sesion o no
+ */
+function mostrarNavBar(booleano, typeOfLog){
+    let btns_logIn = document.querySelectorAll(".loggedIn")
+    let btns_logOut = document.querySelectorAll(".loggedOut");
+
+    if(booleano ===true){
+        for( botonOut of btns_logOut ){
+            if(botonOut === btns_logOut[1]){
+                botonOut.setAttribute("class", `nav-item ${typeOfLog} d-flex justify-content-center align-content-column flex-column align-items-center`)    
+            }else{
+                botonOut.setAttribute('class', `nav-item ${typeOfLog}`)
+            }
+            
+        }
+        for (botonIn of btns_logIn){
+            botonIn.setAttribute("class", "nav-item loggedIn visually-hidden");
+        }
+    }else if(booleano === false){
+        for( botonIn of btns_logIn ){
+            botonIn.setAttribute("class", `nav-item ${typeOfLog}`)
+        }
+        for (botonOut of btns_logOut){
+            botonOut.setAttribute("class", "nav-item loggedOut visually-hidden");
+        }
+    }
+}
+
+/**
+ * @function ejecuta la funcion cuando se termina de cargar toda la informacion de la pagina
+ */
+window.addEventListener("onload",
+    checkerLogIn()
+)
