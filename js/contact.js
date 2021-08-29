ef50eb85cb01ec0fa5e3022777ad5b40918bcc6a
Oscar Mtz de la O <oscarmtz.delao@gmail.com>
2021-08-29 15:03:30 -0500

    refactor | añadi un or a la funcion checkerLogin()
     añadi un or a la funcion checkerLogin() porque habia un bug cuando borrabas la base de datos locales y te mostraba elementos de la navbar que deberian estar ocultos
    -Tambien cambie el acento en el archivo validaciones.js en -Campo valido-

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 let form = document.getElementById('contact-form');
 const myStorage = window.localStorage;
-myStorage.setItem("clients", JSON.stringify([])) //=> {clients: "[]"}
+myStorage.setItem("cliente_Contacto", JSON.stringify([])) //=> {clients: "[]"}
 
 
 /**
@@ -10,7 +10,18 @@ myStorage.setItem("clients", JSON.stringify([])) //=> {clients: "[]"}
 function saveToMyStorage(client){
     let array_clients  = JSON.parse(myStorage.clients) //=> [] es un arreglo vacio o con los clientes en el localStorage
     array_clients.push(client) //[{full_name: 'Pedro', ...}] agrega un nuevo cliente al local storage
-    myStorage.setItem("clients", JSON.stringify(array_clients)) //=> clients: "[{\"full_name\":\"Oscar Martínez\", ...}]" => agrega un elemento al obj de localStorage y lo convierte a JSON
+    myStorage.setItem("cliente_Contacto", JSON.stringify(array_clients)) //=> clients: "[{\"full_name\":\"Oscar Martínez\", ...}]" => agrega un elemento al obj de localStorage y lo convierte a JSON
+}
+
+
+/**
+ * 
+ * @param  {...any} args un arreglo de inputs para limpiar el formulario despues de enviar a localStorage los datos del cliente
+ */
+const clearInputs = (...args)=>{
+    args.forEach((input) =>{
+        input.value = "";
+    })
 }
 
 /**
@@ -24,22 +35,26 @@ function getContactDetails() {
         let email = form.querySelector('[name=email]').value;
         let phone_number = form.querySelector('[name=phone]').value;
         let  text_area = form.querySelector('textarea').value;
-        form.innerHTML += `
-            <a href="mailto:oscarmtz.delao@gmail.com?subject=Mail de Contacto&body=${"Remitente: " +full_name + ' Mensaje: ' + text_area +' – from:' +email}">enviar</a>
-        `;
+        const formInputs = form.querySelectorAll('input'); 
+        // form.innerHTML += `
+        //     <a href="mailto:oscarmtz.delao@gmail.com?subject=Mail de Contacto&body=${"Remitente: " +full_name + ' Mensaje: ' + text_area +' – from:' +email}">enviar</a>
+        // `;
         let potential_client = {
             "full_name" : full_name,
             "email" : email,
             "phone_number" : phone_number,
             "message" : text_area
         }
-        saveToMyStorage(potential_client)
-        
+        saveToMyStorage(potential_client);
+        //clearInputs(formInputs);
         event.preventDefault();
     });
+
     
 };
 
 window.addEventListener('load', (event)=>{
+    myStorage.setItem("cliente_Contacto", JSON.stringify([])) //=> {clients: "[]"}
+
     getContactDetails();
 });
