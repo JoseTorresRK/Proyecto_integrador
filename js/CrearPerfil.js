083315dcab3bd8b973504559622e4a1489e1e05d
José Antonio Torres López <joseanttorres28@gmail.com>
2021-08-29 20:00:48 -0500

    Update||Cambiarelementos

--- js/CrearPerfil.js
+++ js/CrearPerfil.js
@@ -6,3 +6,66 @@ botonesEditar[0].addEventListener("click",function(e){
     crearWidgetCloudinary(false)[0].open();
     
     }, false);
+botonesEditar[1].addEventListener("click",function(e){
+    e.preventDefault();
+    cambiarDesc();
+});
+botonesEditar[2].addEventListener("click",function(e){
+    e.preventDefault();
+    let mensaje=`
+    <strong>Selecciona tus categorias</strong>
+    <div id="Categoria">
+    <div class="form-check">
+    
+    <input class="form-check-input" type="checkbox" value="Albañileria" id="flexCheckDefault">
+    <label class="form-check-label" for="flexCheckDefault">
+      Albañilería
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="Carpinteria" id="flexCheckChecked" >
+    <label class="form-check-label" for="flexCheckChecked">
+      Carpintería
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="Cocina" id="flexCheckChecked" >
+    <label class="form-check-label" for="flexCheckChecked">
+      Cocina
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="Herreria" id="flexCheckChecked" >
+    <label class="form-check-label" for="flexCheckChecked">
+      Herrería
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="Jardineria" id="flexCheckChecked" >
+    <label class="form-check-label" for="flexCheckChecked">
+    Jardinería
+    </label>
+  </div>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="Plomeria" id="flexCheckChecked" >
+    <label class="form-check-label" for="flexCheckChecked">
+      Plomería
+    </label>
+  </div>
+  <div class="row" style="margin-top:3em">
+  <div class="col-6">
+    <button class="btn btn-success"id="formConfirmar" type="submit">Confirmar cambio</button>
+  </div>
+  <div class="col-6">
+    <button class="btn btn-danger" id="formCancelar" type="submit">Cancelar</button>
+  </div>
+  </div>
+  </div>` 
+;  
+    cambiarDatos(1,mensaje,true,"Categoria");
+
+})
+botonesEditar[3].addEventListener("click",function(e){
+    e.preventDefault();
+    let mensaje=``    
+})
